d3de5f005c22aa7764e2338bd89b89c7dbe92942
haridassagarn <46030672+haridassagarn@users.noreply.github.com>
2022-09-06 16:14:49 +0200

    template initilization

---
+++ template_initilization_sample.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class testimpl
+{
+    public:
+    testimpl()
+    {
+        cout<<"in test impl" << endl;
+    }
+    
+};
+
+
+template<typename T>
+class Test
+{
+    
+    private:
+    Test()
+    {
+        
+        cout<< "in test constructor " << endl;
+    }
+    static Test instance;
+    
+};
+
+
+template<typename T>
+Test<T> Test<T>::instance;
+
+template class Test<testimpl>;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
