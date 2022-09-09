3e8d5ccfc6c79fc608accd5f9ae91437a4de6cbc
haridassagarn <46030672+haridassagarn@users.noreply.github.com>
2022-09-09 16:20:24 +0200

    virtual detsructor sample

---
+++ virtual_destrructor_simple_example.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    public:
+    Base()
+    {
+        cout << "in Base() " << endl;
+    }
+    virtual ~Base()
+    {
+         cout << "in ~Base() " << endl;
+    }
+    
+};
+
+class Derived:public Base
+{
+    public:
+    Derived()
+    {
+         cout << "in Derived() " << endl;
+    }
+    ~Derived()
+    {
+         cout << "in ~Derived() " << endl;
+    }
+    
+};
+
+int main()
+{
+   Base* b = new Base();
+   delete b;
+   
+    Derived* d = new Derived();
+    delete d;
+    
+    Base* bd = new Derived();
+    delete bd;
+  return 0;
+}
